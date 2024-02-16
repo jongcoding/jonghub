b12b11edfdb061e06a11d71dd973a7781ab3a709
jongcoding <ialleejy@gmail.com>
2024-02-16 15:43:10 +0900

    hi

--- jump_python/04_/04_3.py
+++ jump_python/04_/04_3.py
@@ -10,6 +10,6 @@ f.close()
 #파일을 쓰기 모드로 열어 내용쓰기
 f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'w')
 for i in range(1, 11):
-    data= "%번째 줄입니다.\n" % i
+    data= "%d번째 줄입니다.\n" % i
     f.write(data)
 f.close()
