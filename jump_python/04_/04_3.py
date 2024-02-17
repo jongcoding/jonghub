ce61341fe681671ae0b69c2102bdf470c0332889
jongcoding <ialleejy@gmail.com>
2024-02-17 14:54:46 +0900

    hi

--- jump_python/04_/04_3.py
+++ jump_python/04_/04_3.py
@@ -13,3 +13,59 @@ for i in range(1, 11):
     data= "%d번째 줄입니다.\n" % i
     f.write(data)
 f.close()
+
+#파일을 읽는 여러 가지 방법
+# readline 함수 이용하기
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+line= f.readline()
+print(line)
+f.close()
+
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+while True:
+    line= f.readline()
+    if not line:break
+    print(line)
+f.close()
+
+#readlines 함수 사용하기
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+lines= f.readlines()
+for line in lines:
+    print(line)
+f.close()
+
+# 줄 바꿈\n 문자 제거하기
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+lines= f.readlines()
+for line in lines:
+    line=line.strip() # 줄 끝의 줄바꿈 문자 제거
+    print(line)
+f.close()
+
+# read 함수 사용하기 -> 파일 내용전체를 문자열로 리턴
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+data=f.read()
+print(data)
+f.close()
+
+# 파일 객체를 for 문과 함께 사용하기
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'r')
+for line in f:
+    print(line)
+f.close()
+
+#파일에 새로운 내용추가하기
+f= open("C:/Users/ialle/OneDrive - 명지대학교/바탕 화면/coding/jonghub/jump_python/04_/새파일.txt", 'a')
+for i in range(11,20):
+    data = "%d번째 줄입니다.\n" % i
+    f.write(data)
+f.close()
+
+# with 문과 함께 사용하기
+f= open("foo.txt", 'w')
+f.write("Life is too short, you need python")
+f.close()
+
+with open("foo.txt", "w") as f:
+    f.write("Life is too short, you need python")
