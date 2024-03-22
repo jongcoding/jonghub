505fe9cb2442bc4b9243c781bf73483c93a5d400
JONG <128033808+jongcoding@users.noreply.github.com>
2024-03-23 02:16:16 +0900

    [update] 파일 편집

--- baekjoon_coding/class3/class3/리모콘.py
+++ baekjoon_coding/class3/class3/리모콘.py
@@ -1,38 +1,38 @@
 #첫쨰줄에 이동하려는 채널이 주어짐
 # 둘쨰줄에 고장난 버튼의수 
 # 셋쨰줄에 고장난 버튼
-def make_combination(posible_buttion,N):
-    count=0
-    leng=list(str(N))
-    numbers=0
-    number=[0]*len(leng)
-    p=0
-    pp=0
-    if len(posible_buttion)<1:
-        return 0
-    else:
-        for i in range(len(leng)):
-            for num in posible_buttion:
-                if int(leng[i])>=num:
-                    posible_num=num
-                    number[i]=[i,num]
-            numbers+=(10**i)*posible_num
-            count+=1
-    for i in range(len(number)):
-        num[-1][1]
-    print(numbers, count)
-    return numbers, count        
+
+from itertools import product
+
+def find_closest_number(target, available_digits):
+    closest_number = None
+    min_difference = float('inf')
+
+    # 가능한 숫자 조합 생성
+    for digits in product(available_digits, repeat=len(str(target))):
+        number = int(''.join(map(str, digits)))
+
+        # 주어진 숫자와의 차이 계산
+        difference = abs(number - target)
+
+        # 가장 작은 차이를 가진 숫자 선택
+        if difference < min_difference:
+            min_difference = difference
+            closest_number = number
+
+    return closest_number
 
 
-N=int(input())
-M=int(input())
-broken_button=set(map(int, input().split()))
-button=set(range(10))
-channel=100
-possible_button=list(button-broken_button)
-main=make_combination(possible_button,N)
-num,count=main
-count_plusminus=abs(N-num)
-print(count_plusminus)
-result=count+count_plusminus
-print(result)
+target =int(input())
+num=int(input())
+if num==0:
+    available_digits=list(range(10))
+else:
+    available_digits =list(set(range(10))-set(map(int, input().split())))
+
+
+closest = find_closest_number(target, available_digits)
+count=abs(target-closest)+len(str(closest))
+if abs(target-100)<count:
+    count=abs(target-100)
+print(count)
