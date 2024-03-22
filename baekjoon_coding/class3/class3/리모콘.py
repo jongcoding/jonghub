9b1d7c4366df6cf591fd7effdd57c391879890f8
JONG <128033808+jongcoding@users.noreply.github.com>
2024-03-23 03:24:26 +0900

    리모콘.py 업데이트

--- baekjoon_coding/class3/class3/리모콘.py
+++ baekjoon_coding/class3/class3/리모콘.py
@@ -2,37 +2,33 @@
 # 둘쨰줄에 고장난 버튼의수 
 # 셋쨰줄에 고장난 버튼
 
-from itertools import product
+def is_possible_channel(channel, broken_buttons):
+    if channel == 0:
+        return '0' not in broken_buttons
 
-def find_closest_number(target, available_digits):
-    closest_number = None
-    min_difference = float('inf')
+    while channel > 0:
+        if str(channel % 10) in broken_buttons:
+            return False
+        channel //= 10
+    return True
 
-    # 가능한 숫자 조합 생성
-    for digits in product(available_digits, repeat=len(str(target))):
-        number = int(''.join(map(str, digits)))
+def min_press_count(target, broken_buttons):
+    min_press_count = abs(target - 100)  # 초기값 설정: +나 - 버튼만을 이용하여 이동할 경우
 
-        # 주어진 숫자와의 차이 계산
-        difference = abs(number - target)
+    for channel in range(500001):  # 0부터 500000 채널을 검사
+        if is_possible_channel(channel, broken_buttons):
+            press_count = abs(target - channel) + len(str(channel))  # 버튼 누름 횟수 계산
+            min_press_count = min(min_press_count, press_count)
 
-        # 가장 작은 차이를 가진 숫자 선택
-        if difference < min_difference:
-            min_difference = difference
-            closest_number = number
+    return min_press_count
 
-    return closest_number
-
-
-target =int(input())
-num=int(input())
-if num==0:
-    available_digits=list(range(10))
+# 입력 받기
+target_channel = int(input())
+broken_button_count = int(input())
+if broken_button_count > 0:
+    broken_buttons = input().split()
 else:
-    available_digits =list(set(range(10))-set(map(int, input().split())))
+    broken_buttons = []
 
-
-closest = find_closest_number(target, available_digits)
-count=abs(target-closest)+len(str(closest))
-if abs(target-100)<count:
-    count=abs(target-100)
-print(count)
+# 결과 출력
+print(min_press_count(target_channel, broken_buttons))
