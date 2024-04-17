d2e478d89de092953a5022215c4c0f6b41d7fe7b
jongcoding <ialleejy@gmail.com>
2024-04-17 17:09:10 +0900

    boj 동적계획법

---
+++ baekjoon_coding/동적계획법1/01타일.py
@@ -0,0 +1,14 @@
+def backtracking(n):
+    dp=[0]*(n+1)
+    dp[1]=1
+    if n>1:
+        dp[2]=2
+    for i in range(3,n+1):
+        dp[i]=(dp[i-1]+dp[i-2])%15746
+
+    return dp[n]
+
+n = int(input())
+
+print(backtracking(n))
+
