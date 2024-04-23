5aec0436a2c2142592d71bbd596d59e914bb9dd6
jongcoding <ialleejy@gmail.com>
2024-04-23 20:34:51 +0900

    boj 동적계획법

---
+++ baekjoon_coding/동적계획법1/가장긴증가하는부분수열.py
@@ -0,0 +1,20 @@
+# 수열 a가 주어졌을 떄 가장 긴 증가하는 부분 수열을 구하는 프로그램을 작성하시오.
+# 입력
+def inc_sub_seq(n, num_list):
+    dp=[[] for _ in range(n)]
+    dp[0]=[num_list[0]]
+    max_count=1
+    for i in range(1, n):
+        max_sub_seq = []  # i번째 요소를 끝으로 하는 가장 긴 부분 수열을 저장할 변수
+        for j in range(i):
+            if num_list[j] < num_list[i] and len(dp[j]) > len(max_sub_seq):
+                max_sub_seq = dp[j].copy()  # j번째 요소까지의 부분 수열을 복사
+        dp[i] = max_sub_seq + [num_list[i]]
+        if max_count<len(dp[i]):
+            max_count=len(dp[i])  # 현재 요소를 부분 수열에 추가
+    print(max_count)
+
+            
+n=int(input())
+num_list=list(map(int, input().split()))
+inc_sub_seq(n,num_list)
