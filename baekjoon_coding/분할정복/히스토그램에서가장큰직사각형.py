86e0a7f7c67daf68d30b7c519d1ef4ff956f2556
jongcoding <ialleejy@gmail.com>
2024-05-20 00:35:55 +0900

    boj 분할정복

--- baekjoon_coding/분할정복/히스토그램에서가장큰직사각형.py
+++ baekjoon_coding/분할정복/히스토그램에서가장큰직사각형.py
@@ -4,36 +4,46 @@
 # 만약 전보다 작다면 현 높이로 가로길이 곱한 값을 직사각형에 넣어버림
 # 만약 전이랑 같아면 해당 높이로 가로길이 곱한 값을 직사가형에 넣음
 
-from collections import deque
-def largest_rectangle(n,h):
-    stack=deque()
-    max_area=0
+import sys
+
+def longest_rect(n,h):
+
+    stack=[]
+    max_rect=0
     index=0
-    while index <n:
-        # 전에 깔려 있던 것보다 크거나 같을때
+    while index<n:
         if not stack or h[stack[-1]]<=h[index]:
-            stack.append(index)
+            stack.append(index) 
             index+=1
-
-        # 전보다 작을 때
         else:
-            top =stack.pop()
-            area=(h[top]*((index-stack[-1]-1) if stack else index))
-            max_area=max(max_area,area)
-
+            top=stack.pop()
+            if not stack:
+                x=index
+            else:
+                x=index-stack[-1]-1
+            y=h[top]
+            area=x*y
+            max_rect=max(area,max_rect)
     while stack:
         top=stack.pop()
-        area=(h[top]*((index-stack[-1]-1) if stack else index))
-        max_area=max(max_area,area)
+        if not stack:
+            x=index
+        else:
+            x=index-stack[-1]-1
+        y=h[top]
+        area=x*y
+        max_rect=max(area,max_rect)       
+    return max_rect
+def main():
+    input =sys.stdin.readline
+    while True:
+        test=list(map(int, input().rstrip().split()))
+        if test==[0]:
+            break
+        n=test[0]
+        h=test[1:]
+        print(longest_rect(n,h))
+    
 
-    return max_area
-            
-while True: 
-
-    input_number= list(map(int, input().split()))
-    if input_number==[0]:
-        exit()    
-    h=deque(input_number)
-    n=h.popleft()
-    print(largest_rectangle(n,h))
-   
+if __name__=='__main__':
+    main()
