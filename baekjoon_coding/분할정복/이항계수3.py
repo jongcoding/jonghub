da2f0578451455b19c55c6606ddc8332ff5c88c3
jongcoding <ialleejy@gmail.com>
2024-05-20 19:50:36 +0900

    boj 분할정복

--- baekjoon_coding/분할정복/이항계수3.py
+++ baekjoon_coding/분할정복/이항계수3.py
@@ -1,30 +1,24 @@
-p = 1000000007
+p=1000000007
 
-def factorial(n):
-    res = 1
-    for i in range(2, n+1):
-        res = (res * i)
+# 페르마의 소정리 a^(p-1)=1 (mod p)
+# a^(p-2)=a^-1 (mod p)
+
+def factorial(x):
+    res=1
+    for i in range(2,x+1):
+        res=(res*i)%p
     return res
+def comb(n,k):
+    n_f=factorial(n)
+    modd=factorial(k)*factorial(n-k)%p
+    inv=mod(modd,p-2)
+    result=n_f*inv%p
+    print(result)
+def mod(a,b):
+    return pow(a,b,p)
 
-def power(x, y):
-    res = 1
-    x = x % p
-    while y > 0:
-        if y % 2:
-            res = (res * x)%p 
-        y = y // 2
-        x = pow(x,2,p)
-    return res
-
-def mod_inverse(x):
-    return power(x, p-2)
-
-def comb(n, k):
-    if k > n:
-        return 0
-    numerator = factorial(n)
-    denominator = (factorial(k) * factorial(n - k))
-    return (numerator * mod_inverse(denominator))%p
-
-n, k = map(int, input().split())
-print(comb(n, k))
+def main():
+    n,k=map(int, input().split())
+    comb(n,k)
+if __name__=='__main__':
+    main()
