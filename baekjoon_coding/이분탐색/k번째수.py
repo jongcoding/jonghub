55171289e5ad13e438735deff1ab5ad164616b37
jongcoding <ialleejy@gmail.com>
2024-05-28 23:49:14 +0900

    boj 이분탐색

--- baekjoon_coding/이분탐색/k번째수.py
+++ baekjoon_coding/이분탐색/k번째수.py
@@ -1,16 +1,17 @@
 n=int(input())
 k=int(input())
-result=0
+a=[[x*y for x in range(1, n+1)] for y in range(1,n+1)]
 start=0
 end=k
-while start <=end:
-    mid = (start+end)//2
+result=0
+while start<= end:
+    mid=(start+end)//2
     temp=0
     for i in range(1, n+1):
         temp+=min(mid//i,n)
-    if temp>= k:
+    if temp>=k:
         result=mid
-        end = mid-1
+        end=mid-1
     else:
         start=mid+1
 print(result)
