1a00d25e1c213cd3ba6e436750bc7c45c7410d83
jongcoding <ialleejy@gmail.com>
2024-06-28 00:05:39 +0900

    update boj

--- baekjoon_coding/동적계획법2/파일합치기.py
+++ baekjoon_coding/동적계획법2/파일합치기.py
@@ -1,13 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-def min_cost_to_merge_files(K, files):
-    # 누적합 계산
+def min_files(K, files):
+    
     prefix_sum = [0] * (K + 1)
     for i in range(1, K + 1):
         prefix_sum[i] = prefix_sum[i - 1] + files[i - 1]
 
-    # DP 테이블 초기화
+    
     dp = [[0] * K for _ in range(K)]
     knuth = [[0] * K for _ in range(K)]
 
@@ -15,7 +15,7 @@ def min_cost_to_merge_files(K, files):
         dp[i][i + 1] = files[i] + files[i + 1]
         knuth[i][i + 1] = i
 
-    # DP 계산
+ 
     for length in range(2, K):
         for i in range(K - length):
             j = i + length
@@ -28,10 +28,10 @@ def min_cost_to_merge_files(K, files):
 
     return dp[0][K - 1]
 
-# 입력 처리
+
 if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
         k = int(input())
         f = list(map(int, input().split()))
-        print(min_cost_to_merge_files(k, f))
+        print(min_files(k, f))
