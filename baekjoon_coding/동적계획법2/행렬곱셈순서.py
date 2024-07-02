0d3b537bbe64591895a54cabc6e3d7e210121c4a
jongcoding <ialleejy@gmail.com>
2024-07-02 17:51:54 +0900

    BOJ 동적계획법2

--- baekjoon_coding/동적계획법2/행렬곱셈순서.py
+++ baekjoon_coding/동적계획법2/행렬곱셈순서.py
@@ -1,3 +1,20 @@
+def cal(matirx):
+    n=len(matrix)
+    dp=[[0] *n for _ in range(n)]
+
+    for lenght in range(2,n):
+        for i in range(1, n-lenght+1):
+            j=i+lenght-1
+            dp[i][j]=float('inf')
+            for k in range(i,j):
+                q=dp[i][k]+dp[k+1][j]+matirx[i-1]*matirx[k]*matrix[j]
+                if q <dp[i][j]:
+                    dp[i][j]=q
+    return dp[1][n-1]
 n=int(input())
+matrix=[]
 for _ in range(n):
     r,c=map(int, input().split())
+    matrix.append(r)
+matrix.append(c)
+print(cal(matrix))
