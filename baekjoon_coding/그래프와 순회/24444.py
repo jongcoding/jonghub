4dc04764cdfad00f05aee2ebd5eb3c928fb100d4
jongcoding <ialleejy@gmail.com>
2024-07-18 23:12:46 +0900

    BOJ 그래프 순회

---
+++ baekjoon_coding/그래프와 순회/24444.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m, r = map(int, input().rstrip().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, input().rstrip().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited = [-1] * (n + 1)
+
+
+def bfs(r):
+    queue = deque([r])
+    visited[r] = 0
+    cnt = 1
+    while queue:
+        node = queue.popleft()
+        graph[node].sort()
+        for neighbor in graph[node]:
+            if visited[neighbor] == -1:
+                queue.append(neighbor)
+                visited[neighbor] = cnt
+                cnt += 1
+
+bfs(r)
+
+for i in range(1, n + 1):
+    print(visited[i]+1)
