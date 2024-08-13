079af4c5017b52c59af808615a16c1df2e1c69a4
jongcoding <ialleejy@gmail.com>
2024-08-13 17:38:14 +0900

    boj

---
+++ baekjoon_coding/bronze/2752.py
@@ -0,0 +1,3 @@
+a,b,c=map(int ,input().split())
+num=[a,b,c]
+print(" ".join(map(str, sorted(num))))
---
+++ baekjoon_coding/그래프와 순회/벽부수고이동하기.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def bfs(mirro,n,m):
+    directions=[(-1,0),(1,0),(0,-1),(0,1)]
+    visited=[[False]*m for _ in range(n)]
+    queue = deque([(0,0)])
+    visited[0][0] = True
+    while queue:
+        x,y = queue.popleft()
+        if x==n-1 and y==m-1:
+            return mirro[x][y]
+        for dx,dy in directions:
+            nx,ny=x+dx,y+dy
+            if 0<=nx <n and 0 <=ny<m and not visited[nx][ny]:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+                mirro[nx][ny]=mirro[x][y]+1
+    return -1
+
+n,m=map(int, input().split())
+mirro=[list(map(int,str(input()))) for _ in range(n)]
+print(bfs(mirro,n,m))
