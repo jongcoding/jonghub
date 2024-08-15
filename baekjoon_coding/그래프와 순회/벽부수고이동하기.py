8f6d01ff391d25a06db759b2f1c173070ecc5eef
jongcoding <ialleejy@gmail.com>
2024-08-15 21:26:46 +0900

    boj 그래프와 순회

--- baekjoon_coding/그래프와 순회/벽부수고이동하기.py
+++ baekjoon_coding/그래프와 순회/벽부수고이동하기.py
@@ -1,22 +1,30 @@
 from collections import deque
 
-def bfs(mirro,n,m):
-    directions=[(-1,0),(1,0),(0,-1),(0,1)]
-    visited=[[False]*m for _ in range(n)]
-    queue = deque([(0,0)])
-    visited[0][0] = True
+def bfs(n, m, grid):
+    
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    queue = deque([(0, 0, 0)])
+    visited[0][0][0] = 1
+    
     while queue:
-        x,y = queue.popleft()
-        if x==n-1 and y==m-1:
-            return mirro[x][y]
-        for dx,dy in directions:
-            nx,ny=x+dx,y+dy
-            if 0<=nx <n and 0 <=ny<m and not visited[nx][ny]:
-                queue.append((nx,ny))
-                visited[nx][ny] = True
-                mirro[nx][ny]=mirro[x][y]+1
+        x, y, wall_broken = queue.popleft()
+        if x == n-1 and y == m-1:
+            return visited[x][y][wall_broken]
+        
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            
+            if 0 <= nx < n and 0 <= ny < m:
+                if grid[nx][ny] == 0 and visited[nx][ny][wall_broken] == 0:
+                    visited[nx][ny][wall_broken] = visited[x][y][wall_broken] + 1
+                    queue.append((nx, ny, wall_broken))
+                if grid[nx][ny] == 1 and wall_broken == 0 and visited[nx][ny][1] == 0:
+                    visited[nx][ny][1] = visited[x][y][wall_broken] + 1
+                    queue.append((nx, ny, 1))
+    
     return -1
 
-n,m=map(int, input().split())
-mirro=[list(map(int,str(input()))) for _ in range(n)]
-print(bfs(mirro,n,m))
+n, m = map(int, input().split())
+grid = [list(map(int, input().strip())) for _ in range(n)]
+print(bfs(n, m, grid))
