79bf7ca723e67b9bbae1d07124e1f0d047c56fd9
Jongcoding <ialleejy@gmail.com>
2024-09-01 19:42:17 +0900

    boj class5

--- baekjoon_coding/class5/2467.py
+++ baekjoon_coding/class5/2467.py
@@ -1,23 +1,22 @@
 import sys
 input=sys.stdin.readline
-def find_closest_to_zero(arr,n):
-    left, right = 0, n- 1
+def find_close(n,solution):
+    left=0
+    right=n-1
     closest_sum = float('inf')
-    result = (0, 0)
-
-    while left < right:
-        current_sum = arr[left] + arr[right]
-
-        if abs(current_sum) < abs(closest_sum):
-            closest_sum = current_sum
-            result = (arr[left], arr[right])
-
-        if current_sum < 0:
-            left += 1
+    result=(0,0)
+    while left<right:
+        current_sum=solution[left]+solution[right]
+        if abs(current_sum)<closest_sum:
+            closest_sum=abs(current_sum)
+            result=(solution[left], solution[right])
+        if current_sum<0:
+            left+=1
         else:
-            right -= 1
-    return result
-n = int(input().rstrip())
-arr = list(map(int, input().rstrip().split()))
-solution = find_closest_to_zero(arr,n)
-print(solution[0], solution[1])
+            right-=1
+    return result[0], result[1]
+n=int(input().rstrip())
+solution=list(map(int, input().split()))
+a,b=find_close(n,solution)
+print(a,b)
+
