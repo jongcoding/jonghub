3e5f131a3281803c2a1e21a5a2838bc90a6fdfc5
Jongcoding <ialleejy@gmail.com>
2024-09-02 22:38:01 +0900

    boj class5

---
+++ baekjoon_coding/class5/16566.py
@@ -0,0 +1,38 @@
+import sys
+import bisect
+input=sys.stdin.readline
+class UnionFind:
+    def __init__(self, size):
+        self.parent = list(range(size))
+    
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        else:
+            self.parent[x] = self.find(self.parent[x]) 
+            return self.parent[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            self.parent[rootX] = rootY
+
+def solve():
+
+    N, M, K = map(int, input().split())
+    minsu = list(map(int, input().split()))
+    chulsu = list(map(int, input().split()))
+
+    minsu.sort()
+    
+    uf = UnionFind(M)
+    for chulsu_card in chulsu:
+        idx = bisect.bisect_right(minsu, chulsu_card)
+        print(idx,"hello")
+        idx = uf.find(idx)
+        print(minsu[idx])
+        if idx + 1 < M:
+            uf.union(idx, idx + 1)
+
+solve()
