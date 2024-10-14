e542fb95f572b3ba620f2bffb6944d42ef26d091
Jongcoding <ialleejy@gmail.com>
2024-10-14 14:14:49 +0900

    boj class3

---
+++ baekjoon_coding/class3/class3/11726.py
@@ -0,0 +1,16 @@
+def fibonachi(n):    
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    
+    elif n>=2:
+        num_list=[0]*(n+1)
+        num_list[1]=1 
+        for i in range(2,n+1):
+            num_list[i]=num_list[i-1]+num_list[i-2]
+        return num_list[n]
+n=int(input())
+print(fibonachi(n+1)%10007)
+
+
